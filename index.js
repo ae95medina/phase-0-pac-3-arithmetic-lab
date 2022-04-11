480528ada6c2be015e5919fbf7efae0a967a82e7
ae95medina <ae95medina@gmail.com>
2022-04-11 10:06:51 -0500

    commit tree 10afb0a30fd0263faaff7fb8e78dcc29c5e490e0

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b){
+    return a + b
+}
+function subtract(b, a){
+    return b - a
+}
+function multiply(a, b){
+    return a * b
+}
+function divide(a, b){
+    return a / b
+}
+function increment(n){
+    return (n + 1)
+}
+function decrement(n){
+    return (n - 1)
+}
+function makeInt(n){
+    console.log(n)
+    return parseInt(n, 10)
+}
+
+function preserveDecimal(string){
+    console.log(string)
+    return parseFloat(string)
+}
